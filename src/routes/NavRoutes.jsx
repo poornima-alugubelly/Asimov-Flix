302af7f2d7f2960ada32921bc2486bc12df5012b
poornima-alugubelly <reddypoornima683@gmail.com>
2022-03-25 19:54:15 +0530

    fix: page links

--- src/routes/NavRoutes.jsx
+++ src/routes/NavRoutes.jsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router-dom";
-import { Playlists } from "../Pages/Playlists/Playlists";
-import { Explore } from "../Pages/Explore/Explore";
-import { SinglePlaylist } from "../Pages/SinglePlaylist/SinglePlaylist";
-import { SingleVideo } from "../Pages/SingleVideo/SingleVideo";
+import { Playlists } from "../pages/Playlists/Playlists";
+import { Explore } from "../pages/Explore/Explore";
+import { SinglePlaylist } from "../pages/SinglePlaylist/SinglePlaylist";
+import { SingleVideo } from "../pages/SingleVideo/SingleVideo";
 export const NavRoutes = () => {
 	return (
 		<Routes>
