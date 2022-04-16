1623c220710b572cefffa4f7adb1a975bdd0a103
poornima-alugubelly <reddypoornima683@gmail.com>
2022-04-16 13:21:14 +0530

    feat:added tool tip , stopped propagation in remove video

--- src/pages/SinglePlaylist/components/PlaylistVideo.jsx
+++ src/pages/SinglePlaylist/components/PlaylistVideo.jsx
@@ -66,7 +66,8 @@ export const PlaylistVideo = ({ video, playlistTitle, playlist }) => {
 		""
 	);
 
-	const removeVideoHandler = () => {
+	const removeVideoHandler = (e) => {
+		e.stopPropagation();
 		switch (playlistTitle) {
 			case "Likes":
 				removeFromLikesServerCall();
@@ -102,7 +103,7 @@ export const PlaylistVideo = ({ video, playlistTitle, playlist }) => {
 				<i
 					class="fas fa-trash"
 					role="button"
-					onClick={() => removeVideoHandler()}
+					onClick={(e) => removeVideoHandler(e)}
 				></i>
 			</div>
 		</div>
